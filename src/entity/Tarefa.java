5b8042001419d54a3b5d6586aef762af40eb067f
Válber Laux <valber@ufrrj.br>
2013-12-04 16:48:51 -0200

    primeiros testes

---
+++ src/entity/Tarefa.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Tarefa {
+	
+	private String descricao;
+	private DateFormat dataCriacao;
+	private DateFormat dataConclusao;
+	
+	
+
+
+}
